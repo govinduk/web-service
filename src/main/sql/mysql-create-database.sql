fe0aa5020344d2456da6a12e4481ab3cbdde8366
kolorobot <rafal.borowiec@gmail.com>
2014-04-03 10:43:39 +0200

    Switched DB implementation to HSQL, so the application is self-contained.

--- src/main/sql/mysql-create-database.sql
+++
@@ -1,8 +0,0 @@
--- Create database
-CREATE DATABASE incidents CHARACTER SET utf8 COLLATE utf8_general_ci;
-
--- Create user
-
-USE incidents;
-CREATE USER 'user'@'localhost' IDENTIFIED BY 'demo';
-GRANT ALL PRIVILEGES ON incidents.* TO 'user'@'localhost';
